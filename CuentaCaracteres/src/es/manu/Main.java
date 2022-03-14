699d147375334ac749abcd0b93a5c0fdfc4277c5
Manuel José Tornero <manueljosetornero@hotmail.com>
2022-03-14 10:40:23 +0100

    contadores klfjalfja

---
+++ CuentaCaracteres/src/es/manu/Main.java
@@ -0,0 +1,23 @@
+package es.manu;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String textico;
+        char letrica;
+        int contador = 0;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Dame un texto: ");
+        textico = entrada.nextLine();
+
+        System.out.print("Dame una letrica: ");
+        letrica = entrada.nextLine().charAt(0);
+
+        for (int i = 0; i < textico.length(); i++) {
+            contador++;
+        }
+    }
+}
